38ea4a717ec8b163e8096ae98c01db9669c986cf
myselfrashid <rashidali.jmi@gmail.com>
2023-07-15 23:46:09 +0530

    first commit

---
+++ Pages/script.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const links = document.querySelectorAll('.nav-links');
+
+toggleBtn.addEventListener('click', () => {
+    nav.classList.toggle('true');
+    nav.setAttribute("style", "display:flex")
+
+    links.forEach((link, index) => {
+        if (link.style.animation){
+            link.style.animation = '';
+        }
+        else{
+            link.style.animation = `listfade 1s ease-in ${index / 100}s`
+        }
+    });
+    toggleBtn.classList.toggle('toggle');
+})
