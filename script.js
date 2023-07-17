661b7fbe5f4b3e161342a5dbebbdad75d50f0ea9
myselfrashid <rashidali.jmi@gmail.com>
2023-07-17 20:45:25 +0530

    ui changes

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const toggleBtn = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
-const links = document.querySelectorAll('.nav-links');
+const toggleBtn = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const links = document.querySelectorAll(".nav-links");
 
-toggleBtn.addEventListener('click', () => {
-    nav.classList.toggle('true');
-    nav.setAttribute("style", "display:flex")
+toggleBtn.addEventListener("click", () => {
+  nav.classList.toggle("true");
+  nav.setAttribute("style", "display:flex");
 
-    links.forEach((link, index) => {
-        if (link.style.animation){
-            link.style.animation = '';
-        }
-        else{
-            link.style.animation = `listfade 1s ease-in ${index / 100}s`
-        }
+  links.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `listfade 1s ease-in ${index / 100}s`;
+    }
+  });
+  toggleBtn.classList.toggle("toggle");
+});
 
-    });
-    toggleBtn.classList.toggle('toggle');
-})
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
